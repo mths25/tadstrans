009779b475cc7351bf05dd388bafad8eeeddc52a
Rhonner <rhonner.matheus@gmail.com>
2021-03-14 13:57:07 -0300

    adicionado primeira classe impl incompleta de multa e adicionado dependencias no pom

---
+++ tadstran/src/main/java/br/com/transtads/service/impl/MultaImpl.java
@@ -0,0 +1,99 @@
+package br.com.transtads.service.impl;
+
+import java.util.List;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+
+import br.com.transtads.response.MultaResponse;
+
+public class MultaImpl {
+
+	private static String path = "/Multa";
+
+	public MultaImpl() {
+	}
+
+	public List<MultaResponse> getListaMulta() {
+		ResponseConfiguration responseConfig = new ResponseConfiguration(path);
+		Response response = responseConfig.getClient().get();
+		if (response.getStatus() == 200) {
+			List<MultaResponse> retorno = response.readEntity(new GenericType<List<MultaResponse>>() {
+			});
+			return retorno;
+		} else {
+			String retorno = response.readEntity(String.class);
+			return null;
+		}
+	}
+
+	public List<MultaResponse> getListaMultaGenericSearch(String search) {
+		ResponseConfiguration responseConfig = new ResponseConfiguration(path + '/' +"genericSearch"+'/'+ search);
+		Response response = responseConfig.getClient().get();
+		if (response.getStatus() == 200) {
+			List<MultaResponse> retorno = response.readEntity(new GenericType<List<MultaResponse>>() {
+			});
+			return retorno;
+		} else {
+			String retorno = response.readEntity(String.class);
+			return null;
+		}
+	}
+	
+	public MultaResponse getMulta(int id) {
+		ResponseConfiguration responseConfig = new ResponseConfiguration(path + '/' + id);
+		Response response = responseConfig.getClient().get();
+		if (response.getStatus() == 200) {
+			MultaResponse retorno = response.readEntity(MultaResponse.class);
+			return retorno;
+		} else {
+			String retorno = response.readEntity(String.class);
+			return null;
+		}
+	}
+
+	public Boolean inserirMulta(MultaResponse Multa) {
+		ResponseConfiguration responseConfig = new ResponseConfiguration(path + "/inserir");
+		if (!Multa.getDescricao().isEmpty() && !Multa.getDocumento().isEmpty()) {
+			Response response = responseConfig.getClient().post(Entity.json(Multa));
+			if (response.getStatus() == 200) {
+				return true;
+			} else {
+				String retorno = response.readEntity(String.class);
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public Boolean alterarMulta(MultaResponse Multa) {
+		ResponseConfiguration responseConfig = new ResponseConfiguration(path + "/alterar");
+		if (!Multa.getDescricao().isEmpty() && !Multa.getDocumento().isEmpty()) {
+			Response response = responseConfig.getClient().put(Entity.json(Multa));
+			if (response.getStatus() == 200) {
+				return true;
+			} else {
+				String retorno = response.readEntity(String.class);
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public MultaResponse excluirMulta(int id) {
+		ResponseConfiguration responseConfig = new ResponseConfiguration(path + "/excluir/" + id);
+		Response response = responseConfig.getClient().delete();
+		if (response.getStatus() == 200) {
+			MultaResponse retorno = response.readEntity(MultaResponse.class);
+			return retorno;
+		} else {
+			String retorno = response.readEntity(String.class);
+			return null;
+		}
+	}
+
+	
+}
---
+++ tadstran/src/main/java/br/com/transtads/service/impl/ResponseConfiguration.java
@@ -0,0 +1,21 @@
+package br.com.transtads.service.impl;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.plugins.providers.jackson.ResteasyJackson2Provider;
+
+public class ResponseConfiguration {
+	private String path;
+	private Builder client;
+	
+	public ResponseConfiguration(String endPoint) {
+		path = "http://localhost:8180/tadstran/webresources" + endPoint;
+		client = ClientBuilder.newClient().register(ResteasyJackson2Provider.class).target(path).request(MediaType.APPLICATION_JSON);
+	}
+	
+	public Builder getClient() {
+		return this.client;
+	}
+}
